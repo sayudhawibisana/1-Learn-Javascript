c8f248af6fb303f5650649d2a8cfdd817d4f7c12
sayudhawibisana <sayudhawibisana@gmail.com>
2021-07-12 18:24:03 +0700

    Working With Classes

--- 2-Modal-Window/script.js
+++ 2-Modal-Window/script.js
@@ -3,4 +3,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  // console.log(`Button ${i} clicked`);
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
