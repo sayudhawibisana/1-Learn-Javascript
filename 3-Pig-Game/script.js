5ed9420dc515b46c5763da7e096baa8ab06740fc
sayudhawibisana <sayudhawibisana@gmail.com>
2021-07-14 07:58:59 +0700

    Rolling the Dice

--- 3-Pig-Game/script.js
+++ 3-Pig-Game/script.js
@@ -1,11 +1,40 @@
 'use strict';
 
-// Selecting elements
+// -- Variable: Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-// Starting conditions
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// -- Info: Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// -- Info: Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  // 1. Generating a random dice Rolling
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // -- NOTE: CHANGE LATER
+  } else {
+    // Switch to next player
+    currentScore = 0;
+    current0El.textContent = currentScore;
+  }
+});
