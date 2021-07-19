e86cd11c04a802c211ca6a1de69a706b2a88ca0f
sayudhawibisana <sayudhawibisana@gmail.com>
2021-07-19 19:52:31 +0700

    Switching the active player

--- 3-Pig-Game/script.js
+++ 3-Pig-Game/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// -----------------------------------------------------------------------------
 // -- Variable: Selecting elements
-const score0El = document.querySelector('#score--0');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -10,14 +13,20 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+// -----------------------------------------------------------------------------
 
+// -----------------------------------------------------------------------------
 // -- Info: Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+// -----------------------------------------------------------------------------
 
+// -----------------------------------------------------------------------------
 // -- Info: Rolling dice functionality
 btnRoll.addEventListener('click', () => {
   // 1. Generating a random dice Rolling
@@ -31,10 +40,15 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // -- NOTE: CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
-    current0El.textContent = currentScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
+// -----------------------------------------------------------------------------
